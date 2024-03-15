ffadb67b3c6fd91d771276d51f8cf8def8fce427
DavLivesey <DaLivesey@yandex.ru>
2024-03-15 23:13:03 +0300

    Начало работы над кнопками

--- config.py
+++ config.py
@@ -10,18 +10,3 @@ HOST = os.getenv('HOST')
 PG_PSWD = os.getenv('PG_PSWD')
 PG_USER = os.getenv('PG_USER')
 
-
-
-
-
-
-'''from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import SecretStr
-
-
-class Settings(BaseSettings):
-    bot_token: SecretStr
-    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
-
-
-config = Settings()'''
